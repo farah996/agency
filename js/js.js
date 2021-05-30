65003e0f5ebbb1c0de23090ccc5510e4cb62760e
farah996 <almahadeenfarah996@gmail.com>
2021-05-30 21:25:36 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,28 @@
+// لتغير خصائص القائمة عند النزول إلى الأسفل noTransparrent إضافة الخاصية 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {
+        $("#navBar").addClass("noTransparrent");
+    } else {
+        $("#navBar").removeClass("noTransparrent");
+    }
+});
+
+// تحرك الصفحة بين عناصر القائمة ضمن فترة زمنية محددة
+$(document).ready(function() {
+    $("a.scroll").on('click', function(event) {
+
+        var hash = this.hash;
+
+        $('html, body').animate({ scrollTop: $(hash).offset().top - 100 }, 800, function() {});
+
+    });
+
+    // countTo تطبيق التابع
+    $('.timer').countTo();
+
+
+    $(function() {
+        $("#commentForm").validate();
+    });
+
+});
